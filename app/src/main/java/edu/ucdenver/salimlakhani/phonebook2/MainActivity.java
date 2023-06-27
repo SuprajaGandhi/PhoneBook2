b21d1be1c121127d0c6eb8096391270f1a6e9b2f
Salim Lakhani <salim.lakhani@ucdenver.edu>
2023-06-27 15:27:49 -0600

    Initial commit

---
+++ app/src/main/java/edu/ucdenver/salimlakhani/phonebook2/MainActivity.java
@@ -0,0 +1,95 @@
+package edu.ucdenver.salimlakhani.phonebook2;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.ucdenver.salimlakhani.phonebook2.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private ArrayList<Contact> list;
+    private ContactAdapter contactAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddContactDialog addContactDialog = new AddContactDialog();
+                addContactDialog.show(getSupportFragmentManager(), "");
+
+            }
+        });
+
+        list = new ArrayList<Contact>();
+        contactAdapter = new ContactAdapter(this, list);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+
+        binding.content.recyclerView.setLayoutManager(layoutManager);
+        binding.content.recyclerView.addItemDecoration(
+                new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        binding.content.recyclerView.setAdapter(contactAdapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return true;
+    }
+
+    public void addContact (Contact contact) {
+        list.add (contact);
+        Log.i ("info", "Contact Name:" + contact.getName());
+        contactAdapter.notifyDataSetChanged();
+    }
+}
